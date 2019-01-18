8ee62420190ef8e444bf08cb3699590cb1feb1a9
k <k@DESKTOP-37KMLI4>
2019-01-18 18:01:20 +0900

    love

--- JAVA/src/ch01/b_test.java
+++ JAVA/src/ch01/b_test.java
@@ -4,6 +4,6 @@ public class b_test {
 
 	public static void main(String[] args) {
 		System.out.println("hello gitworld2");
-		System.out.println("gota getetetetetet");
+		System.out.println("gota getetetetdfdfetet");
 	}
 }
